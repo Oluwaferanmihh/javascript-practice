df5bcfadf3eaf354cd48ae46e4aa84c6c110e725
Oluwaferanmihh <104370294+Oluwaferanmihh@users.noreply.github.com>
2022-09-07 17:31:53 +0100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,11 @@
+function demo(){
+    document.getElementById("demo").innerHTML = Date();
+}
+function darktheme(){
+    document.getElementById("demo2").style.backgroundColor="black";
+    document.getElementById("demo2").style.color="white";
+}
+function lighttheme(){
+    document.getElementById("demo2").style.backgroundColor="white";
+    document.getElementById("demo2").style.color="black";
+}
